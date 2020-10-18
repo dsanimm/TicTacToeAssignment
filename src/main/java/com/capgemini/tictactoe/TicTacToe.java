2d666d4a59edf846093485685c1b9f5812becb11
dsanimm <dsanimm@gmail.com>
2020-10-18 23:49:26 +0530

    Added method to choose X or O

--- src/main/java/com/capgemini/tictactoe/TicTacToe.java
+++ src/main/java/com/capgemini/tictactoe/TicTacToe.java
@@ -1,6 +1,10 @@
 package com.capgemini.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToe {
+	static Scanner sc = new Scanner(System.in);
+	private static char choice;
 
 	public TicTacToe() {
 		char board[] = new char[11];
@@ -11,4 +15,27 @@ public class TicTacToe {
 	public static void main(String[] args) {
 		TicTacToe ticTacToe = new TicTacToe();
 	}
+
+	private static char[] createBoard() {
+		char board[] = new char[11];
+		for (int i = 1; i < 11; i++)
+			board[i] = ' ';
+		return board;
+	}
+
+	private static char[] choiceBoard() {
+		System.out.println("Choose X or O");
+		choice = sc.next().charAt(0);
+	}
+
+	public static void main(String[] args) {
+		TicTacToe obj = new TicTacToe();
+		char[] board = obj.createBoard();
+
+		System.out.println("Choose position for X or O");
+		int pos = sc.nextInt();
+		board = obj.choiceBoard();
+
+	}
+
 }
