17a241c1e0506f527bc711095a3f0e28e552e82f
dsanimm <dsanimm@gmail.com>
2020-10-19 00:34:47 +0530

    Added checkwin and checkdraw method

--- src/main/java/com/capgemini/tictactoe/TicTacToe.java
+++ src/main/java/com/capgemini/tictactoe/TicTacToe.java
@@ -55,6 +55,63 @@ public class TicTacToe {
 		return board;
 	}
 
+	private static boolean checkWin(char[] board) {
+		for (int a = 0; a < 8; a++) {
+			StringBuilder line = null;
+			StringBuilder winx = new StringBuilder().append(choice).append(choice).append(choice);
+			char compLetter = (choice == 'O') ? 'X' : 'O';
+			StringBuilder wino = new StringBuilder().append(compLetter).append(compLetter).append(compLetter);
+			switch (a) {
+			case 0:
+				line = new StringBuilder().append(board[1]).append(board[2]).append(board[3]);
+				break;
+			case 1:
+				line = new StringBuilder().append(board[4]).append(board[5]).append(board[6]);
+				break;
+			case 2:
+				line = new StringBuilder().append(board[7]).append(board[8]).append(board[9]);
+				break;
+			case 3:
+				line = new StringBuilder().append(board[1]).append(board[4]).append(board[7]);
+				break;
+			case 4:
+				line = new StringBuilder().append(board[2]).append(board[5]).append(board[8]);
+				break;
+			case 5:
+				line = new StringBuilder().append(board[3]).append(board[6]).append(board[9]);
+				break;
+			case 6:
+				line = new StringBuilder().append(board[1]).append(board[5]).append(board[9]);
+				break;
+			case 7:
+				line = new StringBuilder().append(board[3]).append(board[5]).append(board[7]);
+				break;
+			}
+			if (line.toString().equals(winx.toString()) || line.toString().equals(wino.toString())) {
+				if (line.toString().equals(winx.toString()))
+					System.out.println("Player Win");
+				else
+					System.out.println("Computer Win");
+
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private static boolean checkDraw(char[] board) {
+		boolean flag = true;
+		for (int i = 1; i < board.length; i++) {
+			if (board[i] == ' ')
+				flag = false;
+		}
+
+		if (flag == true)
+			System.out.println("Draw");
+
+		return flag;
+	}
+
 	public static void main(String[] args) {
 		TicTacToe ticTacToe = new TicTacToe();
 		char[] board = ticTacToe.createBoard();
@@ -77,20 +134,19 @@ public class TicTacToe {
 			System.out.println("tails. Computer plays first");
 			coinwin = 0;
 		}
-
-		if (coinwin == 1) {
+		while (!checkWin(board) && !checkDraw(board) && coinwin == 1) {
 
 			choice = 'X';
-			board = ticTacToe.userChoice(board,coinwin);
+			board = ticTacToe.userChoice(board, coinwin);
 			ticTacToe.printBoard(board);
 		}
 
-		else {
+		while (!checkWin(board) && !checkDraw(board) && coinwin == 0) {
 			choice = 'O';
-			board = ticTacToe.userChoice(board,coinwin);
+			board = ticTacToe.userChoice(board, coinwin);
 			ticTacToe.printBoard(board);
 		}
-
+		if (checkDraw(board))
+			System.out.println("draw");
 	}
-
 }
