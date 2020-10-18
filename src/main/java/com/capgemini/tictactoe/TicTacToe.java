ad360153e8dfe09ecdd97c9139d3b1ab5299916a
dsanimm <dsanimm@gmail.com>
2020-10-19 00:28:32 +0530

    Added coin toss to choose whose chance

--- src/main/java/com/capgemini/tictactoe/TicTacToe.java
+++ src/main/java/com/capgemini/tictactoe/TicTacToe.java
@@ -13,36 +13,25 @@ public class TicTacToe {
 			board[i] = ' ';
 	}
 
-	private static char[] createBoard() {
+	private char[] createBoard() {
 		char board[] = new char[11];
 		for (int i = 1; i < 11; i++)
 			board[i] = ' ';
 		return board;
 	}
 
-	private static char[] choiceBoard(char[] board) {
-		System.out.println("Choose X or O");
-		choice = sc.next().charAt(0);
-		System.out.println("Choose position for X or O");
-		pos = sc.nextInt();
-		if (board[pos] == ' ') {
+	private static char[] choiceBoard(char[] board, char choice, int pos, int coin) {
+		if (coin == 1)
 			board[pos] = choice;
-			return board;
-		} else
-			choiceBoard(board);
+		char compLetter = (choice == 'O') ? 'X' : 'O';
+		// computer plays here
+		if (coin == 0)
+			board[pos] = choice;
 		return board;
-	}
-
-	public static void main(String[] args) {
-		TicTacToe ticTacToe = new TicTacToe();
-		char[] board = ticTacToe.createBoard();
-		ticTacToe.printBoard(board);
-		board = ticTacToe.choiceBoard(board);
-		ticTacToe.printBoard(board);
 
 	}
 
-	private static void printBoard(char[] board) {
+	private void printBoard(char[] board) {
 		for (int i = 1; i < 10; i++) {
 			System.out.print("[" + board[i] + "]");
 			if (i % 3 == 0) {
@@ -50,6 +39,58 @@ public class TicTacToe {
 				System.out.println("---------");
 			}
 		}
+	}
+
+	private char[] userChoice(char[] board, int coin) {
+		if (choice != 'X' && choice != 'O') {
+			System.out.println("Choose X or O");
+			choice = sc.next().charAt(0);
+		}
+		System.out.println("Choose position");
+		int pos = sc.nextInt();
+		if (board[pos] == ' ')
+			board = TicTacToe.choiceBoard(board, choice, pos, coin);
+		else
+			System.out.println("Wrong Choice");
+		return board;
+	}
+
+	public static void main(String[] args) {
+		TicTacToe ticTacToe = new TicTacToe();
+		char[] board = ticTacToe.createBoard();
+		ticTacToe.printBoard(board);
+		System.out.println("choose heads or tails");
+		String coin = sc.next();
+		int randToss = (int) (2 * Math.random());
+		int coinwin = 0;
+		System.out.println(randToss);
+		if (coin.equals("head") && randToss == 0) {
+			System.out.println("heads. Player plays first");
+			coinwin = 1;
+		} else if (coin.equals("tail") && randToss == 0) {
+			System.out.println("heads. Computer plays first");
+			coinwin = 0;
+		} else if (coin.equals("tail") && randToss == 1) {
+			System.out.println("tails. Player plays first");
+			coinwin = 1;
+		} else if (coin.equals("head") && randToss == 1) {
+			System.out.println("tails. Computer plays first");
+			coinwin = 0;
+		}
+
+		if (coinwin == 1) {
+
+			choice = 'X';
+			board = ticTacToe.userChoice(board,coinwin);
+			ticTacToe.printBoard(board);
+		}
+
+		else {
+			choice = 'O';
+			board = ticTacToe.userChoice(board,coinwin);
+			ticTacToe.printBoard(board);
+		}
+
 	}
 
 }
